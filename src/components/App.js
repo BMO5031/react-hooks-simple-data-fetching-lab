a2c812793da3b04a904e2724f998108e00296738
VALHALLA-GANGSTER <+>
2023-07-21 16:26:30 +0300

    fetch commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetching the random dog image from the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        // Once the response is received, set the image URL and mark loading as complete
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+        setIsLoading(false); // In case of an error, still mark loading as complete
+      });
+  }, []); // The empty dependency array ensures that this useEffect callback only runs once on component mount
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+};
+
+export default App;
